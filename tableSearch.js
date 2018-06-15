fc5f6354a6108ad5e06ef0f77a248c495daf1a50
VanessaVanG <vkshiro@live.com>
2018-06-15 17:18:26 -0400

    Add more filters and Refactor

--- tableSearch.js
+++ tableSearch.js
@@ -1,44 +1,44 @@
-//get references to the tbody element, imput field, and button
-var $tbody = document.querySelector("tbody");
-var $datetimeInput = document.querySelector("#datetime");
-var $searchBtn = document.querySelector("#search");
+const tbody = document.querySelector('tbody');
+const input = document.querySelector('#Search');
+const dropdown = document.querySelector('.dropdown-menu');
+let filteredSightings = dataSet;
 
-//add an event listener to the searchBtn, call handleSearchButtonClick when clicked
-$searchBtn.addEventListener("click", handleSearchButtonClick);
-
-// set filtered data to unfiltered data initially
-var filteredSightings = dataSet; 
+//add event listener to dropdown buttons
+dropdown.addEventListener('click', e => {
+  //remove whitespace and lowercase the user's search term
+  const filterValue = input.value.trim().toLowerCase();
+  const button = e.target;
+  if (button.tagName === 'BUTTON') {
+    let searchCategory = button.value;
+    filteredSightings = dataSet.filter(sighting => {
+      let sightingFiltered = sighting[searchCategory].toLowerCase();
+      return sightingFiltered === filterValue;
+    });
+  renderTable();
+  }
+});
 
 //renderTable renders the filteredData to the tbody
 function renderTable() {
-  $tbody.innerHTML = "";
-  for (var i = 0; i < filteredSightings.length; i++) {
+  tbody.innerHTML = "";
+  for (let i = 0; i < filteredSightings.length; i++) {
     //get the current data object and its fields
-    var sighting = filteredSightings[i];
-    var fields = Object.keys(sighting);
-    //creat a new row and set index
-    var $row = $tbody.insertRow(i);
+    let sighting = filteredSightings[i];
+    let fields = Object.keys(sighting);
+    //create a new row and set index
+    let row = tbody.insertRow(i);
+
+    //for each field create a new cell and set its inner text to
+    //be the current value at the current field
     for (let j = 0; j < fields.length; j++) {
-      // For every field in the address object, create a new cell at set its inner text to be the current value at the current address's field
-      var field = fields[j];
-      var $cell = $row.insertCell(j);
-      $cell.innerText = sighting[field];
+      let field = fields[j];
+      let cell = row.insertCell(j);
+      cell.innerText = sighting[field];
     }
   }
 }
 
-function handleSearchButtonClick() {
-  var filterDT = $datetimeInput.value.trim();
-  
-  //set the filteredSightings to the sightings that match the filter
-  filteredSightings = dataSet.filter(function(sighting) {
-    var sightingDT = sighting.datetime.toLowerCase();
-
-    //if true, add sighting to the filtered sightings
-    return sightingDT === filterDT;
-  });
-  renderTable();
-}
-
 //Render the table for the first time on page load
 renderTable();
+
+  
